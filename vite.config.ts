e9ef0d89f24c4b293d0b8ebb6cf5e16a299b762a
lehungvinh2002-beep <lehungvinh2002@gmail.com>
2026-04-21 14:03:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: '/capybara/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: '/capybara/',
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
-  },
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
 });
